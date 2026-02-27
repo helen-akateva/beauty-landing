09214906e61eec32ba0f81dd936b4cffbe955390
helen-akateva <akateva17@gmail.com>
2026-02-27 12:30:13 +0200

    feat: implementation and refinement of the Services section

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ import './src/styles/main.css';
 // Імпорт HTML секцій як рядків (Vite ?raw)
 import headerHTML from './src/sections/header/header.html?raw';
 import heroHTML from './src/sections/hero/hero.html?raw';
+import servicesHTML from './src/sections/services/services.html?raw';
 import footerHTML from './src/sections/footer/footer.html?raw';
 
 // Вставка секцій в DOM
 document.querySelector('#header').innerHTML = headerHTML;
 document.querySelector('#hero').innerHTML = heroHTML;
+document.querySelector('#services').innerHTML = servicesHTML;
 document.querySelector('#footer').innerHTML = footerHTML;
-
-// JS модулі (підключай після вставки HTML в DOM)
-import './src/js/burger.js';
